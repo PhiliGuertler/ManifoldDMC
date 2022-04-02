d9740e17ebadce0320f7cfb7dbb1cd728119a8ab
Philipp Guertler <phili.gue@hotmail.de>
2022-04-02 21:33:08 +0200

    replaced glm's color space functions with inline code On some machines, these glm functions would not work with CUDA correctly

--- LazyEngine/LazyEngine/src/LazyEngine/Renderer/Materials/Colors.h
+++ LazyEngine/LazyEngine/src/LazyEngine/Renderer/Materials/Colors.h
@@ -4,7 +4,6 @@
 #include "LazyEngine/Core/Constants.h"
 #include "LazyEngine/platform/CUDA/CUDAUtils.h"
 #include <glm/glm.hpp>
-#include <glm/gtx/color_space.hpp>
 
 namespace LazyEngine {
 
@@ -14,7 +13,47 @@ namespace LazyEngine {
 		 *	converts an HSV-color into an RGB-color
 		 */
 		HostDevice static inline glm::vec3 HSVtoRGB(const glm::vec3& hsv) {
-			return glm::rgbColor(hsv);
+			float r, g, b;
+
+			float f = hsv.x / 60.0f;
+			float hi = floorf(f);
+			f = f - hi;
+			float p = hsv.z * (1 - hsv.y);
+			float q = hsv.z * (1 - hsv.y * f);
+			float t = hsv.z * (1 - hsv.y * (1 - f));
+
+			if (hi == 0.0f || hi == 6.0f) {
+				r = hsv.z;
+				g = t;
+				b = p;
+			}
+			else if (hi == 1.0f) {
+				r = q;
+				g = hsv.z;
+				b = p;
+			}
+			else if (hi == 2.0f) {
+				r = p;
+				g = hsv.z;
+				b = t;
+			}
+			else if (hi == 3.0f) {
+				r = p;
+				g = q;
+				b = hsv.z;
+			}
+			else if (hi == 4.0f) {
+				r = t;
+				g = p;
+				b = hsv.z;
+			}
+			else {
+				r = hsv.z;
+				g = p;
+				b = q;
+			}
+			return glm::vec3(r, g, b);
+
 		}
 
 		/**
@@ -28,7 +67,50 @@ namespace LazyEngine {
 		 *	converts an RGB-color into an HSV-color
 		 */
 		HostDevice static inline glm::vec3 RGBtoHSV(const glm::vec3& rgb) {
-			return glm::hsvColor(rgb);
+			glm::vec3 out;
+
+			float min = rgb.r < rgb.g ? rgb.r : rgb.g;
+			min = min < rgb.b ? min : rgb.b;
+
+			float max = rgb.r > rgb.g ? rgb.r : rgb.g;
+			max = max > rgb.b ? max : rgb.b;
+
+			out.z = max;                                // v
+			float delta = max - min;
+			if (delta < 0.00001)
+			{
+				out.y = 0;
+				out.x = 0; // undefined, maybe nan?
+				return out;
+			}
+			if (max > 0.0) { // NOTE: if Max is == 0, this divide would cause a crash
+				out.y = (delta / max);                  // s
+			}
+			else {
+				// if max is 0, then r = g = b = 0              
+				// s = 0, h is undefined
+				out.y = 0.0;
+				out.x = NAN;                            // its now undefined
+				return out;
+			}
+			if (rgb.r >= max) {                           // > is bogus, just keeps compilor happy
+				out.x = (rgb.g - rgb.b) / delta;        // between yellow & magenta
+			} else {
+				if (rgb.g >= max) {
+					out.x = 2.f + (rgb.b - rgb.r) / delta;  // between cyan & yellow
+				}
+				else {
+					out.x = 4.f + (rgb.r - rgb.g) / delta;  // between magenta & cyan
+				}
+			}
+
+			out.x *= 60.f;                              // degrees
+
+			if (out.x < 0.f) {
+				out.x += 360.f;
+			}
+
+			return out;
 		}
 
 		/**
@@ -42,7 +124,7 @@ namespace LazyEngine {
 		 *	returns the luminosity of an RGB color
 		 */
 		HostDevice static inline float getLuminosityRGB(const glm::vec3& rgb) {
-			return glm::luminosity(rgb);
+			return 0.299 * rgb.r + 0.587 * rgb.g + 0.114 * rgb.b;
 		}
 
 		/**
